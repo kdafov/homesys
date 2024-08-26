0d9141856c8730cf2c3e1cf58b1e4deecff2ff0a
forfail <business.dafov@gmail.com>
2024-08-26 17:45:57 +0100

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,85 @@
+'use client'
+
 import Image from "next/image";
+import recipes from "@/data/data.json";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [recipe, setRecipe] = useState(-1);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-white">
+
+      {recipe === -1 ? <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        {["main", "breakfast", 'side', 'sauce', 'dessert'].map((type) => (<>
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-10">{type[0].toUpperCase() + type.slice(1) + 's'}</h1>
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mb-10">
+            {recipes.filter(recipe => recipe.type === type).map((recipe) => (
+              <a key={recipe.id} className="group cursor-pointer" onClick={() => setRecipe(recipe.id)}>
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                  <img
+                    alt={recipe.name}
+                    src={recipe.img}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                  <div className="absolute inset-0 bg-gray-800 bg-opacity-50 flex items-end p-4">
+                    <p className="text-2xl font-medium text-white">{recipe.name}</p>
+                  </div>
+                </div>
+              </a>
+            ))}
+          </div>
+        </>))}
+
+
+      </div> 
+        :
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-10">{recipe !== -1 && recipes[recipe].name}</h2>
+        <Image src={recipe !== -1 && '/' + recipes[recipe].img} alt="Image of food" width={500} height={60} className="rounded-lg bg-gray-200 object-cover w-2/3 h-96"/>
+        
+        {recipe !== -1 && recipes[recipe].ingredients.length !== 0 &&
+          <div className="mx-auto my-10">
+            <span className="text-2xl font-semibold">Ingredients:</span>
+            <ul className="mx-5 list-disc pt-3 text-lg">
+              {recipe !== -1 && recipes[recipe].ingredients.map((ingredient, i) => (
+                <li key={i}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+        }
+
+        {recipe !== -1 && recipes[recipe].method.length !== 0 &&
+          <div className="mx-auto my-10">
+            <span className="text-2xl font-semibold">Preparation:</span>
+            <ol className="mx-5 list-decimal pt-3 text-lg">
+              {recipe !== -1 && recipes[recipe].method.map((step, i) => (
+                <li key={i}>{step}</li>
+              ))}
+            </ol>
+          </div>
+        }
+
+        <div className="mx-auto my-10">
+          {recipe !== -1 && recipes[recipe].matches.length !== 0 ?
+            <span className="text-2xl font-semibold">A good match with:</span>
+              :
+            <span className="text-2xl font-semibold">Perfect to enjoy on its own</span>
+          }
+          <div className="pt-3">
+            {recipe !== -1 && recipes[recipe].matches.map((item, i) => (
+              (item[0] !== -1 ?
+                <span key={i} onClick={() => setRecipe(item[0])} class="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 mr-2 text-s font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10 cursor-pointer">{item[1]}</span>
+                  :
+                <span key={i} class="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 mr-2 text-s font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">{item[1]}</span>
+              )
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <button onClick={() => setRecipe(-1)} class="rounded-md bg-indigo-600 mt-4 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Main menu</button>
+      </div> }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
